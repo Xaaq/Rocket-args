37e0ce124479fe7bfaf279779b579c0ef7b8bbb4
Amadeusz Hercog <xaaq333@gmail.com>
2020-02-14 20:51:43 +0100

    reorder setup.py args

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ setup_requirements = ["pytest-runner"]
 test_requirements = ["pytest>=3"]
 
 setup(
+    version="0.1.0",
     author="Amadeusz Hercog",
     author_email="xaaq333@gmail.com",
     python_requires=">=3.6",
@@ -26,17 +27,18 @@ setup(
         "Programming Language :: Python :: 3.8",
     ],
     description="Make your arg parsing even more declarative!",
-    install_requires=requirements,
     license="MIT license",
     long_description=readme,
-    include_package_data=True,
-    keywords="rocket_args",
+    long_description_content_type="text/markdown",
     name="rocket_args",
     packages=["rocket_args"],
-    setup_requires=setup_requirements,
-    test_suite="tests",
-    tests_require=test_requirements,
     url="https://github.com/Xaaq/rocket_args",
-    version="0.1.0",
+    install_requires=requirements,
+    # TODO: check what below args do
+    setup_requires=setup_requirements,
+    tests_require=test_requirements,
+    include_package_data=True,
+    keywords="rocket_args",
+    test_suite="tests",
     zip_safe=False,
 )
