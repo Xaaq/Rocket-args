1d890c6191550d2b5d2f17b09b23c24c465a4dbf
Amadeusz Hercog <xaaq333@gmail.com>
2020-02-17 18:34:21 +0100

    put test methods of RocketBase into a class

--- tests/test_rocket_base.py
+++ tests/test_rocket_base.py
@@ -2,31 +2,33 @@ from rocket_args.main import RocketBase
 from tests.test_get_cmd_line_args import patch_cli_args
 
 
-def test_provided_arguments_return_appropriate_values():
-    class Args(RocketBase):
-        arg_int: int
-        arg_str: str
-        arg_float: float
+class TestParseArgs:
+    @staticmethod
+    def test_provided_arguments_return_appropriate_values():
+        class Args(RocketBase):
+            arg_int: int
+            arg_str: str
+            arg_float: float
 
-    input_args = ["--arg-int", "123", "--arg-str", "abc", "--arg-float", "123.456"]
+        input_args = ["--arg-int", "123", "--arg-str", "abc", "--arg-float", "123.456"]
 
-    with patch_cli_args(input_args):
-        output_args = Args.parse_args()
+        with patch_cli_args(input_args):
+            output_args = Args.parse_args()
 
-    assert output_args.arg_int == 123
-    assert output_args.arg_str == "abc"
-    assert output_args.arg_float == 123.456
+        assert output_args.arg_int == 123
+        assert output_args.arg_str == "abc"
+        assert output_args.arg_float == 123.456
 
+    @staticmethod
+    def test_not_provided_arguments_fallback_to_defaults():
+        class Args(RocketBase):
+            arg_int: int = 123
+            arg_str: str = "abc"
+            arg_float: float = 123.456
 
-def test_not_provided_arguments_fallback_to_defaults():
-    class Args(RocketBase):
-        arg_int: int = 123
-        arg_str: str = "abc"
-        arg_float: float = 123.456
+        with patch_cli_args([]):
+            output_args = Args.parse_args()
 
-    with patch_cli_args([]):
-        output_args = Args.parse_args()
-
-    assert output_args.arg_int == 123
-    assert output_args.arg_str == "abc"
-    assert output_args.arg_float == 123.456
+        assert output_args.arg_int == 123
+        assert output_args.arg_str == "abc"
+        assert output_args.arg_float == 123.456
