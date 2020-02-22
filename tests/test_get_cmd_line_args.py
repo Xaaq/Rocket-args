4f1e9c4a3e4785ca610861944be1dee6907c8c61
Amadeusz Hercog <xaaq333@gmail.com>
2020-02-22 21:31:42 +0100

    fit get_cmd_line_args tests to new changes

--- tests/test_get_cmd_line_args.py
+++ tests/test_get_cmd_line_args.py
@@ -1,23 +1,22 @@
 import pytest
 from _pytest.capture import CaptureFixture
 from factory import Factory, Sequence
-from factory.fuzzy import FuzzyChoice
 
-from rocket_args.utils import ArgData, get_cmd_line_args, var_name_to_arg_name
+from rocket_args.utils import FullArgumentData, get_arg_value_from_namespace, get_cmd_line_args
 from tests.utils import patch_cli_args
 
 
-class ArgDataFactory(Factory):
+class FullArgumentDataFactory(Factory):
     class Meta:
-        model = ArgData
+        model = FullArgumentData
 
-    name: str = Sequence(lambda i: f"test_arg_{i}")
-    is_required: bool = FuzzyChoice((True, False))
+    names: str = Sequence(lambda i: [f"--test-arg-{i}"])
     default: str = Sequence(lambda i: f"value_{i}")
+    help_message: str = Sequence(lambda i: f"Test help message number {i}")
 
 
 def test_provided_arguments_have_appropriate_values() -> None:
-    input_args = [ArgDataFactory(name=f"arg_{i}") for i in range(3)]
+    input_args = [FullArgumentDataFactory(names=[f"--arg-{i}"]) for i in range(3)]
     cli_args = ["--arg-0", "value_0", "--arg-1", "value_1", "--arg-2", "value_2"]
 
     with patch_cli_args(cli_args):
@@ -28,7 +27,7 @@ def test_provided_arguments_have_appropriate_values() -> None:
 
 
 def test_not_provided_required_arguments_display_appropriate_message(capsys: CaptureFixture) -> None:
-    input_args = [ArgDataFactory(is_required=True) for _ in range(3)]
+    input_args = [FullArgumentDataFactory(default=...) for _ in range(3)]
 
     with patch_cli_args([]), pytest.raises(SystemExit):
         get_cmd_line_args(input_args)
@@ -36,23 +35,23 @@ def test_not_provided_required_arguments_display_appropriate_message(capsys: Cap
     output = capsys.readouterr().err
     assert "the following arguments are required" in output
 
-    for arg_name in input_args:
-        cli_arg_name = var_name_to_arg_name(arg_name.name)
-        assert cli_arg_name in output
+    for arg_data in input_args:
+        names = "/".join(arg_data.names)
+        assert names in output
 
 
 def test_not_provided_not_required_arguments_fallback_to_defaults() -> None:
-    input_args = [ArgDataFactory(is_required=False) for _ in range(3)]
+    input_args = [FullArgumentDataFactory() for _ in range(3)]
 
     with patch_cli_args([]):
         args = get_cmd_line_args(input_args)
 
     for arg in input_args:
-        assert args.__getattribute__(arg.name) == arg.default
+        assert get_arg_value_from_namespace(arg.names, args) == arg.default
 
 
 def test_arguments_abbreviations_are_not_allowed(capsys: CaptureFixture) -> None:
-    input_args = [ArgDataFactory(name=f"{name}_arg") for name in ("first", "second", "third")]
+    input_args = [FullArgumentDataFactory(names=[f"--{name}_arg"]) for name in ("first", "second", "third")]
     cli_args = ["--first", "value", "--second", "value", "--third", "value"]
 
     with patch_cli_args(cli_args), pytest.raises(SystemExit):
