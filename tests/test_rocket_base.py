97a5bec6ab21ff20fee1c2eb4d48a2187c19a253
Amadeusz Hercog <xaaq333@gmail.com>
2020-02-22 22:49:15 +0100

    move get_cmd_line_args tests to test_rocket_base.py + add FullArgumentData tests

--- tests/test_rocket_base.py
+++ tests/test_rocket_base.py
@@ -1,3 +1,9 @@
+from typing import List
+
+import pytest
+from _pytest.capture import CaptureFixture
+
+from rocket_args import Argument
 from rocket_args.main import RocketBase
 from tests.utils import patch_cli_args
 
@@ -10,14 +16,31 @@ class TestParseArgs:
             arg_str: str
             arg_float: float
 
-        input_args = ["--arg-int", "123", "--arg-str", "abc", "--arg-float", "123.456"]
+        cli_args = ["--arg-int", "123", "--arg-str", "abc", "--arg-float", "123.456"]
 
-        with patch_cli_args(input_args):
+        with patch_cli_args(cli_args):
             output_args = Args.parse_args()
 
         assert output_args.arg_int == 123
         assert output_args.arg_str == "abc"
         assert output_args.arg_float == 123.456
+
+    # noinspection PyUnresolvedReferences
+    @staticmethod
+    def test_not_provided_required_arguments_display_appropriate_message(capsys: CaptureFixture) -> None:
+        class Args(RocketBase):
+            arg_1: str = Argument(names=["-a1", "--arg-1", "----long-arg-1"])
+            arg_2: str = Argument(names=["-a2", "--arg-2", "----long-arg-2"])
+
+        with patch_cli_args([]), pytest.raises(SystemExit):
+            Args.parse_args()
+
+        output = capsys.readouterr().err
+        assert "the following arguments are required" in output
+
+        for argument in [Args.arg_1, Args.arg_2]:
+            names = "/".join(argument.names)
+            assert names in output
 
     @staticmethod
     def test_not_provided_arguments_fallback_to_defaults() -> None:
@@ -32,6 +55,54 @@ class TestParseArgs:
         assert output_args.arg_int == 123
         assert output_args.arg_str == "abc"
         assert output_args.arg_float == 123.456
+
+    @staticmethod
+    @pytest.mark.parametrize(
+        "cli_args",
+        [["-a", "value"], ["--arg", "value"], ["----long-arg-name", "value"]],
+        ids=["short arg", "long arg", "very long args"],
+    )
+    def test_all_arg_names_can_be_provided_from_cli(cli_args: List[str]) -> None:
+        class Args(RocketBase):
+            arg: str = Argument(names=["-a", "--arg", "----long-arg-name"])
+
+        with patch_cli_args(cli_args):
+            output_args = Args.parse_args()
+
+        assert output_args.arg == "value"
+
+    @staticmethod
+    def test_arguments_abbreviations_are_not_allowed(capsys: CaptureFixture) -> None:
+        class Args(RocketBase):
+            first_arg: str
+            second_arg: str
+            third_arg: str
+
+        cli_args = ["--first", "value", "--second", "value", "--third", "value"]
+
+        with patch_cli_args(cli_args), pytest.raises(SystemExit):
+            Args.parse_args()
+
+        capsys.readouterr()
+
+    # noinspection PyUnresolvedReferences
+    @staticmethod
+    def test_help_message_contains_arguments_data(capsys: CaptureFixture) -> None:
+        class Args(RocketBase):
+            arg_1: str = Argument(names=["-a1", "--arg-1"], help="First argument.")
+            arg_2: str = Argument(names=["-a2", "--arg-2"], help="Second argument.")
+
+        cli_args = ["--help"]
+
+        with patch_cli_args(cli_args), pytest.raises(SystemExit):
+            Args.parse_args()
+
+        output = capsys.readouterr().out
+
+        for argument in [Args.arg_1, Args.arg_2]:
+            for name in argument.names:
+                assert name in output
+            assert argument.help in output
 
 
 class TestRepr:
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,9 +1,10 @@
 from argparse import Namespace
-from typing import Sequence
+from typing import Any, Sequence
 
 import pytest
 
-from rocket_args.utils import get_arg_value_from_namespace, var_name_to_arg_name
+from rocket_args import Argument
+from rocket_args.utils import FullArgumentData, get_arg_value_from_namespace, var_name_to_arg_name
 
 
 def test_var_name_to_arg_name() -> None:
@@ -16,23 +17,23 @@ def test_var_name_to_arg_name() -> None:
 
 @pytest.mark.parametrize(
     "cli_arg_names, namespace",
-    (
-        (["-a"], Namespace(a="value")),
-        (["-a-"], Namespace(a_="value")),
-        (["-arg"], Namespace(arg="value")),
-        (["-arg-"], Namespace(arg_="value")),
-        (["--arg"], Namespace(arg="value")),
-        (["--arg-"], Namespace(arg_="value")),
-        (["-arg-with-dashes"], Namespace(arg_with_dashes="value")),
-        (["--arg-with-dashes"], Namespace(arg_with_dashes="value")),
-        (["----arg-with-dashes"], Namespace(arg_with_dashes="value")),
-        (["-a", "--arg"], Namespace(arg="value")),
-        (["-a", "----arg"], Namespace(arg="value")),
-        (["-a", "--arg-1", "--arg-2"], Namespace(arg_1="value")),
-        (["-a", "--arg-1", "----arg-2"], Namespace(arg_1="value")),
-        (["-a", "----arg-1", "--arg-2"], Namespace(arg_1="value")),
-    ),
-    ids=(
+    [
+        [["-a"], Namespace(a="value")],
+        [["-a-"], Namespace(a_="value")],
+        [["-arg"], Namespace(arg="value")],
+        [["-arg-"], Namespace(arg_="value")],
+        [["--arg"], Namespace(arg="value")],
+        [["--arg-"], Namespace(arg_="value")],
+        [["-arg-with-dashes"], Namespace(arg_with_dashes="value")],
+        [["--arg-with-dashes"], Namespace(arg_with_dashes="value")],
+        [["----arg-with-dashes"], Namespace(arg_with_dashes="value")],
+        [["-a", "--arg"], Namespace(arg="value")],
+        [["-a", "----arg"], Namespace(arg="value")],
+        [["-a", "--arg-1", "--arg-2"], Namespace(arg_1="value")],
+        [["-a", "--arg-1", "----arg-2"], Namespace(arg_1="value")],
+        [["-a", "----arg-1", "--arg-2"], Namespace(arg_1="value")],
+    ],
+    ids=[
         "1-dash short arg name",
         "1-dash short arg name with end dash",
         "1-dash long arg name",
@@ -47,8 +48,48 @@ def test_var_name_to_arg_name() -> None:
         "1, 2 and 2-dash args",
         "1, 2 and 4-dash args",
         "1, 4 and 2-dash args",
-    ),
+    ],
 )
 def test_get_arg_value_from_namespace(cli_arg_names: Sequence[str], namespace: Namespace) -> None:
     result = get_arg_value_from_namespace(cli_arg_names, namespace)
     assert result == "value"
+
+
+class TestFullArgumentData:
+    @staticmethod
+    @pytest.mark.parametrize("default, is_required", [["string", False], [None, False], [..., True]])
+    def test_is_required(default: Any, is_required: bool) -> None:
+        arg_data = FullArgumentData([], default)
+        assert arg_data.is_required == is_required
+
+    @staticmethod
+    def test_from_raw_data() -> None:
+        name = "arg_name"
+        value = "value"
+
+        arg_data = FullArgumentData.from_raw_data(name, value)
+
+        assert arg_data.names == [var_name_to_arg_name(name)]
+        assert arg_data.default == value
+        assert arg_data.help_message is None
+
+    @staticmethod
+    def test_from_user_arg_data_takes_argument_names_if_provided() -> None:
+        argument = Argument(names=["-a", "--arg"], default="value", help="help message")
+
+        arg_data = FullArgumentData.from_user_arg_data("arg_name", argument)
+
+        assert arg_data.names == argument.names
+        assert arg_data.default == argument.default
+        assert arg_data.help_message == argument.help
+
+    @staticmethod
+    def test_from_user_arg_data_takes_var_name_when_names_not_provided() -> None:
+        var_name = "arg_name"
+        argument = Argument(default="value", help="help message")
+
+        arg_data = FullArgumentData.from_user_arg_data(var_name, argument)
+
+        assert arg_data.names == [var_name_to_arg_name(var_name)]
+        assert arg_data.default == argument.default
+        assert arg_data.help_message == argument.help
