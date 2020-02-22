0566ac39dfb776f6eb400af77269034ea368e02e
Amadeusz Hercog <xaaq333@gmail.com>
2020-02-22 21:30:58 +0100

    add tests for get_arg_value_from_namespace

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,9 @@
-from rocket_args.utils import var_name_to_arg_name
+from argparse import Namespace
+from typing import Sequence
+
+import pytest
+
+from rocket_args.utils import get_arg_value_from_namespace, var_name_to_arg_name
 
 
 def test_var_name_to_arg_name() -> None:
@@ -7,3 +12,43 @@ def test_var_name_to_arg_name() -> None:
 
     actual_value = var_name_to_arg_name(input_arg)
     assert actual_value == expected_arg
+
+
+@pytest.mark.parametrize(
+    "cli_arg_names, namespace",
+    (
+        (["-a"], Namespace(a="value")),
+        (["-a-"], Namespace(a_="value")),
+        (["-arg"], Namespace(arg="value")),
+        (["-arg-"], Namespace(arg_="value")),
+        (["--arg"], Namespace(arg="value")),
+        (["--arg-"], Namespace(arg_="value")),
+        (["-arg-with-dashes"], Namespace(arg_with_dashes="value")),
+        (["--arg-with-dashes"], Namespace(arg_with_dashes="value")),
+        (["----arg-with-dashes"], Namespace(arg_with_dashes="value")),
+        (["-a", "--arg"], Namespace(arg="value")),
+        (["-a", "----arg"], Namespace(arg="value")),
+        (["-a", "--arg-1", "--arg-2"], Namespace(arg_1="value")),
+        (["-a", "--arg-1", "----arg-2"], Namespace(arg_1="value")),
+        (["-a", "----arg-1", "--arg-2"], Namespace(arg_1="value")),
+    ),
+    ids=(
+        "1-dash short arg name",
+        "1-dash short arg name with end dash",
+        "1-dash long arg name",
+        "1-dash long arg name with end dash",
+        "2-dash arg name",
+        "2-dash arg name with end dash",
+        "1-dash arg with dashes",
+        "2-dash arg with dashes",
+        "4-dash arg with dashes",
+        "1 and 2-dash args",
+        "1 and 4-dash args",
+        "1, 2 and 2-dash args",
+        "1, 2 and 4-dash args",
+        "1, 4 and 2-dash args",
+    ),
+)
+def test_get_arg_value_from_namespace(cli_arg_names: Sequence[str], namespace: Namespace) -> None:
+    result = get_arg_value_from_namespace(cli_arg_names, namespace)
+    assert result == "value"
