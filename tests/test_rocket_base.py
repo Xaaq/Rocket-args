72448202a8d331cd7bbca13e21f57282ffc420d6
Amadeusz Hercog <xaaq333@gmail.com>
2020-02-23 13:28:44 +0100

    add test for defaults provided by Argument

--- tests/test_rocket_base.py
+++ tests/test_rocket_base.py
@@ -43,11 +43,25 @@ class TestParseArgs:
             assert names in output
 
     @staticmethod
-    def test_not_provided_arguments_fallback_to_defaults() -> None:
+    def test_not_provided_arguments_with_direct_defaults_set_fallback_to_them() -> None:
         class Args(RocketBase):
             arg_int: int = 123
             arg_str: str = "abc"
             arg_float: float = 123.456
+
+        with patch_cli_args([]):
+            output_args = Args.parse_args()
+
+        assert output_args.arg_int == 123
+        assert output_args.arg_str == "abc"
+        assert output_args.arg_float == 123.456
+
+    @staticmethod
+    def test_not_provided_arguments_with_indirect_defaults_set_fallback_to_them() -> None:
+        class Args(RocketBase):
+            arg_int: int = Argument(default=123)
+            arg_str: str = Argument(default="abc")
+            arg_float: float = Argument(default=123.456)
 
         with patch_cli_args([]):
             output_args = Args.parse_args()
