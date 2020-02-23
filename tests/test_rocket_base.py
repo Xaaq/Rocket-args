b2b94dd37dc5ade17bfcfd9c816fa2771f350e1a
Amadeusz Hercog <xaaq333@gmail.com>
2020-02-23 15:05:53 +0100

    separate tests using Argument and not using it

--- tests/test_rocket_base.py
+++ tests/test_rocket_base.py
@@ -8,7 +8,7 @@ from rocket_args.main import RocketBase
 from tests.utils import patch_cli_args
 
 
-class TestParseArgs:
+class TestParseArgsWithoutUsingArgument:
     @staticmethod
     def test_provided_arguments_return_appropriate_values() -> None:
         class Args(RocketBase):
@@ -19,6 +19,80 @@ class TestParseArgs:
         cli_args = ["--arg-int", "123", "--arg-str", "abc", "--arg-float", "123.456"]
 
         with patch_cli_args(cli_args):
+            output_args = Args.parse_args()
+
+        assert output_args.arg_int == 123
+        assert output_args.arg_str == "abc"
+        assert output_args.arg_float == 123.456
+
+    @staticmethod
+    def test_not_provided_arguments_fallback_to_defaults() -> None:
+        class Args(RocketBase):
+            arg_int: int = 123
+            arg_str: str = "abc"
+            arg_float: float = 123.456
+
+        with patch_cli_args([]):
+            output_args = Args.parse_args()
+
+        assert output_args.arg_int == 123
+        assert output_args.arg_str == "abc"
+        assert output_args.arg_float == 123.456
+
+    @staticmethod
+    def test_not_provided_required_arguments_display_appropriate_message(capsys: CaptureFixture) -> None:
+        class Args(RocketBase):
+            arg_1: str
+            arg_2: str
+
+        with patch_cli_args([]), pytest.raises(SystemExit):
+            Args.parse_args()
+
+        output = capsys.readouterr().err
+        assert "the following arguments are required" in output
+
+        for name in ["--arg-1", "--arg-2"]:
+            assert name in output
+
+    @staticmethod
+    def test_arguments_abbreviations_are_not_allowed(capsys: CaptureFixture) -> None:
+        class Args(RocketBase):
+            first_arg: str
+            second_arg: str
+            third_arg: str
+
+        cli_args = ["--first", "value", "--second", "value", "--third", "value"]
+
+        with patch_cli_args(cli_args), pytest.raises(SystemExit):
+            Args.parse_args()
+
+        capsys.readouterr()
+
+
+class TestParseArgsUsingArgument:
+    @staticmethod
+    @pytest.mark.parametrize(
+        "cli_args",
+        [["-a", "value"], ["--arg", "value"], ["----long-arg-name", "value"]],
+        ids=["short arg", "long arg", "very long args"],
+    )
+    def test_all_arg_names_can_be_provided_from_cli(cli_args: List[str]) -> None:
+        class Args(RocketBase):
+            arg: str = Argument(names=["-a", "--arg", "----long-arg-name"])
+
+        with patch_cli_args(cli_args):
+            output_args = Args.parse_args()
+
+        assert output_args.arg == "value"
+
+    @staticmethod
+    def test_not_provided_arguments_fallback_to_defaults() -> None:
+        class Args(RocketBase):
+            arg_int: int = Argument(default=123)
+            arg_str: str = Argument(default="abc")
+            arg_float: float = Argument(default=123.456)
+
+        with patch_cli_args([]):
             output_args = Args.parse_args()
 
         assert output_args.arg_int == 123
@@ -41,63 +115,6 @@ class TestParseArgs:
         for argument in [Args.arg_1, Args.arg_2]:
             names = "/".join(argument.names)
             assert names in output
-
-    @staticmethod
-    def test_not_provided_arguments_with_direct_defaults_set_fallback_to_them() -> None:
-        class Args(RocketBase):
-            arg_int: int = 123
-            arg_str: str = "abc"
-            arg_float: float = 123.456
-
-        with patch_cli_args([]):
-            output_args = Args.parse_args()
-
-        assert output_args.arg_int == 123
-        assert output_args.arg_str == "abc"
-        assert output_args.arg_float == 123.456
-
-    @staticmethod
-    def test_not_provided_arguments_with_indirect_defaults_set_fallback_to_them() -> None:
-        class Args(RocketBase):
-            arg_int: int = Argument(default=123)
-            arg_str: str = Argument(default="abc")
-            arg_float: float = Argument(default=123.456)
-
-        with patch_cli_args([]):
-            output_args = Args.parse_args()
-
-        assert output_args.arg_int == 123
-        assert output_args.arg_str == "abc"
-        assert output_args.arg_float == 123.456
-
-    @staticmethod
-    @pytest.mark.parametrize(
-        "cli_args",
-        [["-a", "value"], ["--arg", "value"], ["----long-arg-name", "value"]],
-        ids=["short arg", "long arg", "very long args"],
-    )
-    def test_all_arg_names_can_be_provided_from_cli(cli_args: List[str]) -> None:
-        class Args(RocketBase):
-            arg: str = Argument(names=["-a", "--arg", "----long-arg-name"])
-
-        with patch_cli_args(cli_args):
-            output_args = Args.parse_args()
-
-        assert output_args.arg == "value"
-
-    @staticmethod
-    def test_arguments_abbreviations_are_not_allowed(capsys: CaptureFixture) -> None:
-        class Args(RocketBase):
-            first_arg: str
-            second_arg: str
-            third_arg: str
-
-        cli_args = ["--first", "value", "--second", "value", "--third", "value"]
-
-        with patch_cli_args(cli_args), pytest.raises(SystemExit):
-            Args.parse_args()
-
-        capsys.readouterr()
 
     # noinspection PyUnresolvedReferences
     @staticmethod
